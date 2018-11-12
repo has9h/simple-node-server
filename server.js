f402d1eb46bdff741c61a127151c1f6499c309cb
Hasnain Hossain <has9h2@gmail.com>
2018-11-12 03:05:34 +0000

    Express.js

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 var http = require('http');
-var fs = require('fs');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', function(err, data){
-        if(err){
-            return console.log("File system read error");
-        }
-        res.end(data);
-    });
-    // res.end("Hello World\n");
-  });
-  server.listen(process.env.PORT, process.env.IP, function(){
-    console.log('Server running');
-  });
+// Using express to start the server and add routes
+var express = require("express");
+var app = express();            // The express app
+var server = http.Server(app);  // Adding the server
+
+app.get('/', function(request, response){       // '/' is the root URL
+  response.sendFile(__dirname+'/index.html');   //''
+});
+
+server.listen(process.env.PORT, process.env.IP, function(){
+  console.log('Server running');
+});
+
+// Remove the file serving code and add the express.js code
+// var fs = require('fs');
+//   var server = http.createServer(function(req, res){
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     fs.readFile('index.html', function(err, data){
+//         if(err){
+//             return console.log("File system read error");
+//         }
+//         res.end(data);
+//     });
+//     // res.end("Hello World\n");
+//   });
+//   server.listen(process.env.PORT, process.env.IP, function(){
+//     console.log('Server running');
+//   });

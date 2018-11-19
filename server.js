5223f1408dcc7d9b38f6a17e71135635beea55ef
Hasnain Hossain <has9h2@gmail.com>
2018-11-19 03:26:37 +0000

    MongoDB Addition

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ var app = express();            // The express app
 var server = http.Server(app);  // Adding the server
 var bodyParser = require('body-parser');
 
+// Import MongoDB:
+var mongo = require('mongodb');
+// Specifying port for C9
+var db;                                                   // Database client if connection is successfull
+var db_url = "mongodb://" + process.env.IP + ":27017";    //process.env specifies the port of the workspace(?)
+// For local instance:
+// var db_url = "mongodb://localhost:27017";
+
+mongo.MongoClient.connect(db_url, {useNewUrlParser: true}, function(err, client){
+  if(err){
+    console.log('Could not connect to MongoDB');
+  }else{
+    db = client.db('node-cw9');
+  }
+});
+
+// Saving user data
+var save = function(form_data){
+  db.createCollection('articles', function(err, collection){
+    console.log("Collection Created");
+  });
+}
+
 // Configuring app for body-parser
 app.use(bodyParser.json());
 // Ensuring it uses encoded URLs
@@ -30,7 +53,7 @@ app.post('/article/create', function(request, response){
   if(!request.body.title){
     return response.status(400).json({error:"Please add a title"});
   }
-  article.push(request.body);
+  article.push(request.body);                         //This should be saved to the database
   return response.status(200).json({message: "Article successfully created"});
 });
 
